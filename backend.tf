a35c101f0a82706d611302f842e384ccaf851ed6
Bkoji1150 <85324828+Bkoji1150@users.noreply.github.com>
2022-02-16 22:13:30 -0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,13 +1,12 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "tf-project-omega"
-#     key            = "haplet.tfstate"
-#     profile = "default"
-#     encrypt = true
-#     region         = "us-west-2"
-#     dynamodb_table = "tf-project-omega"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "tf-project-omega"
+    key            = "haplet.tfstate"
+    encrypt = true
+    region         = "us-west-2"
+    
+  }
+}
 
 # data "terraform_remote_state" operational_environment {
 #   backend = "s3"
